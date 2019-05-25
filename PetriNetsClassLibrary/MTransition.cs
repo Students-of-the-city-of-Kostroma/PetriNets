1a8df0d198e65f7da6c9e0140a71c402d781aaef
denis <kapu4inko@gmail.com>
2019-05-25 19:43:06 +0300

    Добавлен склелет класса MTransition

---
+++ PetriNetsClassLibrary/MTransition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetriNetsClassLibrary
+{
+	class MTransition
+	{
+		List<MPlace> inPlaces;
+		List<MPlace> outPlaces;
+		bool isEnable;
+
+		public void inPlacesAdd(MPlace mPlaceIn)
+		{
+			inPlaces.Add(mPlaceIn);
+		}
+
+		public void outPlacesAdd(MPlace mPlaceOut)
+		{
+			outPlaces.Add(mPlaceOut);
+		}
+
+		public string label
+		{
+			get;set;
+		}
+
+		MTransition(string _label)
+		{
+			isEnable = false;
+			label = _label;
+		}
+
+	}
+}
