3cc6229daa967f7c526a488e2832f235b774b5cc
denis <kapu4inko@gmail.com>
2019-05-25 19:49:09 +0300

    Создан вспомогательный класс Edge

---
+++ PetriNetsClassLibrary/Edge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetriNetsClassLibrary
+{
+	class Edge
+	{
+		public Tuple<MPlace, MTransition> edge
+		{
+			get; set;
+		}
+	}
+}
