ff9bfe96ae35139cfe95867a0e723d36d9153946
denis <kapu4inko@gmail.com>
2019-05-25 19:38:49 +0300

    Добавление скелета класса MPlace

---
+++ PetriNetsClassLibrary/MPlace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetriNetsClassLibrary
+{
+	class MPlace
+	{
+
+		public uint tokens
+		{
+			get; set;
+		}
+
+		public string label
+		{
+			get; set;
+		}
+
+		MPlace(string _label)
+		{
+			label = _label;
+		}
+	}
+}
