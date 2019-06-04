6b2139c9cdb44fefdc2afca78ef8faf1864ca3c6
denis <kapu4inko@gmail.com>
2019-06-04 14:39:12 +0300

    Добавлены контекстные меню для разных объекто Сетей Петри

--- PetriNets/PetriNet.Designer.cs
+++ PetriNets/PetriNet.Designer.cs
@@ -31,12 +31,25 @@
 			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PetriNet));
 			this.bindingNavigator1 = new System.Windows.Forms.BindingNavigator(this.components);
+			this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.tbEdit = new System.Windows.Forms.ToolStripMenuItem();
+			this.cmPlace = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.cmArc = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.cmTransition = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.miEditTransition = new System.Windows.Forms.ToolStripMenuItem();
+			this.tsmDeleteTransition = new System.Windows.Forms.ToolStripMenuItem();
+			this.tsmEditWeight = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.tsmDeleteArc = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+			this.tsmDeletePlace = new System.Windows.Forms.ToolStripMenuItem();
+			this.editNumberOfTokens = new System.Windows.Forms.ToolStripMenuItem();
 			this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
 			this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
 			this.pause = new System.Windows.Forms.ToolStripButton();
 			this.stop = new System.Windows.Forms.ToolStripButton();
 			this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
-			this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
 			this.circle = new System.Windows.Forms.ToolStripButton();
 			this.rectangle = new System.Windows.Forms.ToolStripButton();
 			this.line = new System.Windows.Forms.ToolStripButton();
@@ -44,6 +57,9 @@
 			this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
 			((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
 			this.bindingNavigator1.SuspendLayout();
+			this.cmPlace.SuspendLayout();
+			this.cmArc.SuspendLayout();
+			this.cmTransition.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// bindingNavigator1
@@ -73,6 +89,104 @@
 			this.bindingNavigator1.Size = new System.Drawing.Size(800, 25);
 			this.bindingNavigator1.TabIndex = 0;
 			this.bindingNavigator1.Text = "bindingNavigator1";
+			// 
+			// bindingNavigatorSeparator2
+			// 
+			this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+			this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
+			// 
+			// tbEdit
+			// 
+			this.tbEdit.Name = "tbEdit";
+			this.tbEdit.Size = new System.Drawing.Size(242, 22);
+			this.tbEdit.Text = "Изменить название";
+			this.tbEdit.Click += new System.EventHandler(this.tbEdit_Click);
+			// 
+			// cmPlace
+			// 
+			this.cmPlace.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tbEdit,
+            this.editNumberOfTokens,
+            this.toolStripSeparator3,
+            this.tsmDeletePlace});
+			this.cmPlace.Name = "cmPlace";
+			this.cmPlace.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+			this.cmPlace.Size = new System.Drawing.Size(243, 76);
+			// 
+			// cmArc
+			// 
+			this.cmArc.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmEditWeight,
+            this.toolStripSeparator1,
+            this.tsmDeleteArc});
+			this.cmArc.Name = "cmArc";
+			this.cmArc.Size = new System.Drawing.Size(150, 54);
+			// 
+			// cmTransition
+			// 
+			this.cmTransition.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miEditTransition,
+            this.toolStripSeparator2,
+            this.tsmDeleteTransition});
+			this.cmTransition.Name = "cmTransition";
+			this.cmTransition.Size = new System.Drawing.Size(182, 76);
+			// 
+			// miEditTransition
+			// 
+			this.miEditTransition.Name = "miEditTransition";
+			this.miEditTransition.Size = new System.Drawing.Size(181, 22);
+			this.miEditTransition.Text = "Изменить название";
+			this.miEditTransition.Click += new System.EventHandler(this.tbEdit_Click);
+			// 
+			// tsmDeleteTransition
+			// 
+			this.tsmDeleteTransition.Name = "tsmDeleteTransition";
+			this.tsmDeleteTransition.Size = new System.Drawing.Size(181, 22);
+			this.tsmDeleteTransition.Text = "Удалить";
+			this.tsmDeleteTransition.Click += new System.EventHandler(this.tsmDelete_Click);
+			// 
+			// tsmEditWeight
+			// 
+			this.tsmEditWeight.Name = "tsmEditWeight";
+			this.tsmEditWeight.Size = new System.Drawing.Size(149, 22);
+			this.tsmEditWeight.Text = "Изменить вес";
+			this.tsmEditWeight.Click += new System.EventHandler(this.tsmEditWeight_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(146, 6);
+			// 
+			// tsmDeleteArc
+			// 
+			this.tsmDeleteArc.Name = "tsmDeleteArc";
+			this.tsmDeleteArc.Size = new System.Drawing.Size(149, 22);
+			this.tsmDeleteArc.Text = "Удалить";
+			this.tsmDeleteArc.Click += new System.EventHandler(this.tsmDelete_Click);
+			// 
+			// toolStripSeparator2
+			// 
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			this.toolStripSeparator2.Size = new System.Drawing.Size(178, 6);
+			// 
+			// toolStripSeparator3
+			// 
+			this.toolStripSeparator3.Name = "toolStripSeparator3";
+			this.toolStripSeparator3.Size = new System.Drawing.Size(239, 6);
+			// 
+			// tsmDeletePlace
+			// 
+			this.tsmDeletePlace.Name = "tsmDeletePlace";
+			this.tsmDeletePlace.Size = new System.Drawing.Size(242, 22);
+			this.tsmDeletePlace.Text = "Удалить";
+			this.tsmDeletePlace.Click += new System.EventHandler(this.tsmDelete_Click);
+			// 
+			// editNumberOfTokens
+			// 
+			this.editNumberOfTokens.Name = "editNumberOfTokens";
+			this.editNumberOfTokens.Size = new System.Drawing.Size(242, 22);
+			this.editNumberOfTokens.Text = "Изменить количество токенов";
+			this.editNumberOfTokens.Click += new System.EventHandler(this.editNumberOfTokens_Click);
 			// 
 			// bindingNavigatorMoveFirstItem
 			// 
@@ -118,11 +232,6 @@
 			this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
 			this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
 			this.bindingNavigatorMoveLastItem.Text = "Переместить в конец";
-			// 
-			// bindingNavigatorSeparator2
-			// 
-			this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
-			this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
 			// 
 			// circle
 			// 
@@ -180,6 +289,7 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
 			this.Controls.Add(this.bindingNavigator1);
+			this.DoubleBuffered = true;
 			this.Name = "PetriNet";
 			this.Text = "Сети Петри";
 			this.Click += new System.EventHandler(this.CanvasClick);
@@ -187,6 +297,9 @@
 			((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).EndInit();
 			this.bindingNavigator1.ResumeLayout(false);
 			this.bindingNavigator1.PerformLayout();
+			this.cmPlace.ResumeLayout(false);
+			this.cmArc.ResumeLayout(false);
+			this.cmTransition.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -206,5 +319,18 @@
 		private System.Windows.Forms.ToolStripButton stop;
 		private System.Windows.Forms.ToolStripButton toolStripButton1;
 		private System.Windows.Forms.ToolStripButton toolStripButton2;
+		private System.Windows.Forms.ToolStripMenuItem tbEdit;
+		private System.Windows.Forms.ContextMenuStrip cmPlace;
+		private System.Windows.Forms.ContextMenuStrip cmArc;
+		private System.Windows.Forms.ContextMenuStrip cmTransition;
+		private System.Windows.Forms.ToolStripMenuItem miEditTransition;
+		private System.Windows.Forms.ToolStripMenuItem tsmDeleteTransition;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+		private System.Windows.Forms.ToolStripMenuItem tsmDeletePlace;
+		private System.Windows.Forms.ToolStripMenuItem tsmEditWeight;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripMenuItem tsmDeleteArc;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+		private System.Windows.Forms.ToolStripMenuItem editNumberOfTokens;
 	}
 }
