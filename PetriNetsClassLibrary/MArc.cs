c53b0aae24bfa52ef4cf3d93d920a5c74688f31a
denis <kapu4inko@gmail.com>
2019-06-08 16:48:17 +0300

    Руководство разработчика для классов-моделей

--- PetriNetsClassLibrary/MArc.cs
+++ PetriNetsClassLibrary/MArc.cs
@@ -4,25 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Класс-модель для представления объекта arc из теории сетей петри
+/// </summary>
 namespace PetriNetsClassLibrary
 {
 	[Serializable]
 	public class MArc : Model
 	{
+		/// <summary>
+		/// Стандартный конструктор. По умолчанию вес арки - 1
+		/// </summary>
 		public MArc()
 		{
 			weight = 1;
 		}
 
+		/// <summary>
+		/// Конструктор арки через объекты классов MPlace и MTransition, т.е с установленной связью
+		/// </summary>
+		/// <param name="mPlace">объект связи Edge типа MPlace</param>
+		/// <param name="mTransition">объект связи Edge типа  MTransition</param>
+		/// <param name="isIn">Входная или выходная арка</param>
 		public MArc(MPlace mPlace, MTransition mTransition, bool isIn)
 		{
 			edge = new Edge(mPlace, mTransition, isIn);
 		}
+
+		/// <summary>
+		/// Свойство обозначающие вес арки
+		/// </summary>
 		public uint weight
 		{
 			get;set;
 		}
 
+		/// <summary>
+		/// Ребро соответсвующее арке
+		/// </summary>
 		public Edge edge
 		{
 			get; set;
--- PetriNetsClassLibrary/MTransition.cs
+++ PetriNetsClassLibrary/MTransition.cs
@@ -4,26 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Класс-модель для представления объекта transition из теории сетей петри
+/// </summary>
 namespace PetriNetsClassLibrary
 {
 	[Serializable]
 	public class MTransition : Model
 	{
+		/// <summary>
+		/// Хранение входных арок MArc в текущий переход MTransition
+		/// </summary>
 		public List<MArc> inPlaces { get; } = new List<MArc>();
+		/// <summary>
+		/// Хранение выходных арок MArc в текущий переход MTransition
+		/// </summary>
 		public List<MArc> outPlaces { get; } = new List<MArc>();
+		/// <summary>
+		/// Это рабочий переход MTransition?
+		/// </summary>
 		public bool isEnable;
 
+		/// <summary>
+		/// Хранение наименование объекта MTransition
+		/// </summary>
 		public string label
 		{
 			get;set;
 		}
 
+		/// <summary>
+		/// Конструктор через наименование объекта. По дефолту переход выключен
+		/// </summary>
+		/// <param name="_label">наименование объекта</param>
 		public MTransition(string _label)
 		{
 			isEnable = false;
 			label = _label;
 		}
 
+		/// <summary>
+		/// Добавление арки MArc в соответсвующий лист Арок: inPlaces или outPlaces 
+		/// </summary>
+		/// <param name="arc">добавляемая арка MArc</param>
+		/// <param name="places">лист арок в который происходит добавление</param>
+		/// <returns>Успешно ли прошло добавление (Если уже есть такая арка, то false)</returns>
 		public bool addArc(MArc arc, List<MArc> places)
 		{
 			foreach(var place in places)
@@ -34,8 +59,6 @@ namespace PetriNetsClassLibrary
 			places.Add(arc);
 			return true;
 		}
-
-
 
 	}
 }
